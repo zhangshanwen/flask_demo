04adeade0c76e8672adc402cf04dab05def1aac1
zhangshanwen <huichiroudeyu@163.com>
2021-08-18 16:00:01 +0800

    fix:     1. migrate manage with config

--- manage.py
+++ manage.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 from migrate.versioning.shell import main
+import json
 
 if __name__ == '__main__':
-    main(repository='migrations', url='mysql+mysqlconnector://root:123456@127.0.0.1:3306/ya', debug='False')
+    with open("config/settings.json") as f:
+        config = json.loads(f.read())
+    mysql_json = config.get("MYSQL", {})
+    db_url = "mysql+mysqlconnector://{user}:{password}@{host}:{port}/{dbname}"
+    db_url = db_url.format(user=mysql_json.get("user"), password=mysql_json.get("password"),
+                           host=mysql_json.get("host"),
+                           port=mysql_json.get("port"), dbname=mysql_json.get("dbname"))
+
+    main(repository='migrations', url=db_url, debug='False')
